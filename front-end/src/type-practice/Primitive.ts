b748e0453a95253d5d9335550d43f2823d372449
Yusuke-Ijima2 <yusuke9251120@gmail.com>
2022-02-27 18:47:50 +0900

    [add]基本的な型について学習

--- front-end/src/type-practice/Primitive.ts
+++ front-end/src/type-practice/Primitive.ts
@@ -55,6 +55,7 @@ let obj: {
 };
 
 //型エイリアス(Type Aliases)
+//typeの先頭は大文字
 type Profile = {
   name: string;
   age: number;
@@ -67,3 +68,70 @@ const myProfile: Profile = {
 };
 //１度型を作ってしまえばtypeofで使いまわせる
 type Profile2 = typeof myProfile;
+
+//unknown型
+//下記のままだと変数numberUnknownの型がわからないから足し算できないかもしれないと判断される
+let numberUnknown: unknown = 0;
+// let sumUnknown = numberUnknown + 10;←エラーになる
+//typeofを使って特定の型であることを確認しながらコードを実行させることを"型ガード"という
+if (typeof numberUnknown === "number") {
+  let sumUnknown = numberUnknown + 10;
+}
+
+//交差型(intersection型)
+type Name = {
+  firstName: string;
+  lastName: string;
+};
+type Age = {
+  age: number;
+};
+const myprofile: Name & Age = {
+  firstName: "優介",
+  lastName: "飯嶋",
+  age: 20,
+};
+
+// /共用体型(union型)
+let value: number | string = 0;
+value = "hoge";
+
+//Literal型
+//string型
+let holiday: "土" | "日" = "日";
+// holiday = "月";←エラー
+
+//number型
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+// month = 13;←エラー
+
+//boolean型
+let TRUE: true = true;
+// TRUE = false;←エラー
+
+//列挙型(enum型)
+//number型
+//最初の要素は0から始まるが最初の要素に任意の値を代入することで変更できる
+enum Months {
+  January = 1, //1
+  February, //2
+  March, //3
+  April, //4
+  May, //5
+  June, //6
+  July, //7
+  August, //8
+  September, //9
+  October, //10
+  November, //11
+  December, //12
+}
+console.log(Months.January); //1
+//string型
+enum Ages {
+  Me = "20歳",
+  Brother = "15歳",
+  Mom = "45歳",
+  Daddy = "50歳",
+}
+console.log(Ages.Brother); //15歳
